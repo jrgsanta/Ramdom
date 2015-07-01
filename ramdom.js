f2c73f2d9292ef3ddcd3183ca00d45518ff20414
jrgsanta <imac@iMac-de-iMac.local>
2015-07-01 15:55:36 +0200

    Random version 2
    Mejorar uso de strings

--- ramdom.js
+++ ramdom.js
@@ -1,7 +1,9 @@
 var numero = Math.random();
+var str = '';
 if (numero <= 0.5){
-  console.log('\n'+numero+' Menor que 0,5 \n');
+  str = ' Menor';
 }
 else {
-  console.log('\n'+numero+' MAyor que 0,5 \n');
+  str = ' Mayor';
 }
+console.log('\n'+ numero + str + ' que 0,5 \n');
